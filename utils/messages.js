e5e933fb94fda579d488c0413a90c978eabfe0b0
Matheus Alencar <malencar@pm.me>
2020-01-21 15:04:57 -0300

    fix: email notifications messages

--- utils/messages.js
+++ utils/messages.js
@@ -35,23 +35,11 @@ module.exports = {
       '{{nickname}}, a submission you participated in was accepted into the registry. Deposits were transfered out of the contract but there may be rewards pending withdrawal for appeal fee contributors.',
     [REMOVAL_ACCEPTED]:
       '{{nickname}}, a removal you participated in was executed and the item was removed from the registry. Deposits were transfered out of the contract but there may be rewards pending withdrawal for appeal fee contributors.',
-    [SUBMISSION_CHALLENGED]: `Hi {{nickname}},
-
- A submission was challenged. click below to view the justification evidence provided by the challenger.`,
-    [REMOVAL_CHALLENGED]: `Hi {{nickname}},
-
- Removal challenged. Click below to view the justification evidence provided by the challenger.`,
-    [EVIDENCE_SUBMITTED]: `Hi {{nickname}},
-
- Someone submitted evidence in a case you are participating. Click below to view it.`,
-    [APPEALED]: `Hi {{nickname}},
-
- The ruling given by the arbitrator has been appealed. Click below view the item status and follow updates.`,
-    [APPEALABLE_RULING]: `Hi {{nickname}},
-
- The arbitrator gave an appealable ruling. Click below to view it and request an appeal if you think it is incorrect.`,
-    [FINAL_RULING]: `Hi {{nickname}},
-
- The ruling given by the arbitrator has been enforced. Deposits left the contract but appeal fee contribution rewards may be available for withdrawal. Click below to view the item.`
+    [SUBMISSION_CHALLENGED]: `Hi {{nickname}}, someone challenged a submission you are subscribed to. Click below to view the justification evidence provided by the challenger.`,
+    [REMOVAL_CHALLENGED]: `Hi {{nickname}}, someone challenged a submission you are subscribed to. Click below to view the justification evidence provided by the challenger.`,
+    [EVIDENCE_SUBMITTED]: `Hi {{nickname}}, new evidence was submitted in a case you are subscribed to. Click below to view it.`,
+    [APPEALABLE_RULING]: `Hi {{nickname}}, the arbitrator gave an appealable ruling. Click below to view it and request an appeal if you think it is incorrect.`,
+    [APPEALED]: `Hi {{nickname}}, the ruling given by the arbitrator has been appealed. Click below view the item status and follow updates.`,
+    [FINAL_RULING]: `Hi {{nickname}}, the ruling given by the arbitrator has been enforced. Deposits left the contract but appeal fee contribution rewards may be available for withdrawal. Click below to view the item.`
   }
 }
