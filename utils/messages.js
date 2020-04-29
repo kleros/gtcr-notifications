d263325ccbd9aa4f5c4c502abc559b2e60dd7788
Matheus <mtsalenc@gmail.com>
2020-04-29 14:11:05 -0300

    fix: add secretlint and format messages

--- utils/messages.js
+++ utils/messages.js
@@ -27,10 +27,14 @@ module.exports = {
     [FINAL_RULING]: 'Ruling enforced.'
   },
   MESSAGES: {
-    [SUBMISSION_PENDING]: '{{nickname}}, a submission you participated in is pending execution. Click below to view its status and execute it to transfer deposits out of the contract.',
-    [REMOVAL_PENDING]: '{{nickname}}, a removal you participated in pending execution. Click below to view its status.',
-    [SUBMISSION_ACCEPTED]: '{{nickname}}, a submission you participated in was accepted into the registry. Deposits were transferred out of the contract but there may be rewards pending withdrawal for appeal fee contributors.',
-    [REMOVAL_ACCEPTED]: '{{nickname}}, a removal you participated in was executed and the item was removed from the registry. Deposits were transferred out of the contract but there may be rewards pending withdrawal for appeal fee contributors.',
+    [SUBMISSION_PENDING]:
+      '{{nickname}}, a submission you participated in is pending execution. Click below to view its status and execute it to transfer deposits out of the contract.',
+    [REMOVAL_PENDING]:
+      '{{nickname}}, a removal you participated in pending execution. Click below to view its status.',
+    [SUBMISSION_ACCEPTED]:
+      '{{nickname}}, a submission you participated in was accepted into the registry. Deposits were transferred out of the contract but there may be rewards pending withdrawal for appeal fee contributors.',
+    [REMOVAL_ACCEPTED]:
+      '{{nickname}}, a removal you participated in was executed and the item was removed from the registry. Deposits were transferred out of the contract but there may be rewards pending withdrawal for appeal fee contributors.',
     [SUBMISSION_CHALLENGED]: `Hi {{nickname}}, someone challenged a submission you are subscribed to. Click below to view the justification evidence provided by the challenger.`,
     [REMOVAL_CHALLENGED]: `Hi {{nickname}}, someone challenged a submission you are subscribed to. Click below to view the justification evidence provided by the challenger.`,
     [EVIDENCE_SUBMITTED]: `Hi {{nickname}}, new evidence was submitted in a case you are subscribed to. Click below to view it.`,
