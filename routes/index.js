f0989b3949f7bcda13f985a0d5af2354b126c30e
mtsalenc <matheus@kleros.io>
2021-07-14 13:57:18 +0100

    fix: missing await in call

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ module.exports = provider => {
   const router = express.Router()
 
   /* GET home page. */
-  router.get('/', function(_, res) {
-    const network = provider.getNetwork()
+  router.get('/', async function(_, res) {
+    const network = await provider.getNetwork()
     res.send(
       `<h1>Curate Notifications</h1> <p>All systems go.</p> <p>${JSON.stringify(
         network
